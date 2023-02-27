2f17bfa76dcdd9bc2ef0b7467f6c5437a0cf4755
KarenFlEs <karen.florian6es@gmail.com>
2023-02-27 09:15:03 -0500

    Added the packages and classes

---
+++ WavesSimulation/app/src/main/java/WavesSimulation/UI/MainApp.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package WavesSimulation.UI;
+
+/**
+ *
+ * @author 2045287
+ */
+public class MainApp {
+    
+    public String getGreeting() {
+        return "Hello World!";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MainApp().getGreeting());
+    }
+    
+}

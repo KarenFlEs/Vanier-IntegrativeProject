a20dc257a6171e928612bb2835a2c8dcc8b2d96a
Steven01231 <dysteven1@gmail.com>
2023-03-17 23:51:57 -0400

    Added modifiers to variables and added a shape in the slit simulation

--- WavesSimulation/app/src/main/java/WavesSimulation/Slits/SlitsController.java
+++ WavesSimulation/app/src/main/java/WavesSimulation/Slits/SlitsController.java
@@ -7,6 +7,8 @@ package WavesSimulation.Slits;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Slider;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -16,6 +18,9 @@ import javafx.stage.Stage;
 public class SlitsController extends Stage {
     
     Stage owner;
+    
+    @FXML
+    private Pane mainScreen;
     
     @FXML
     private CheckBox btn1Slit;
@@ -37,6 +42,18 @@ public class SlitsController extends Stage {
     
     @FXML 
     private Slider sldSeperation;
+    
+    private Rectangle rec = new Rectangle(1000, 1000);
+    
+    @FXML
+    public void initialize(){
+        
+        rec.setLayoutX(200);
+        rec.setLayoutY(200);
+        mainScreen.getChildren().add(rec);
+        
+        
+    }
 
     public SlitsController(Stage owner) {
         this.owner = owner;
--- WavesSimulation/app/src/main/java/WavesSimulation/Slits/SlitsEngine.java
+++ WavesSimulation/app/src/main/java/WavesSimulation/Slits/SlitsEngine.java
@@ -9,11 +9,11 @@ package WavesSimulation.Slits;
  * @author 2045287
  */
 public class SlitsEngine {
-    int frequency;
-    int amplitude;
-    int slitSep;
-    int nbSlits;
-    int slitWid;
+    private int frequency;
+    private int amplitude;
+    private int slitSep;
+    private int nbSlits;
+    private int slitWid;
 
     public SlitsEngine() {
     }
@@ -41,4 +41,46 @@ public class SlitsEngine {
     public boolean isInside(){
         return true; //for now
     }
+
+    public int getFrequency() {
+        return this.frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
+    public int getAmplitude() {
+        return this.amplitude;
+    }
+
+    public void setAmplitude(int amplitude) {
+        this.amplitude = amplitude;
+    }
+
+    public int getSlitSep() {
+        return this.slitSep;
+    }
+
+    public void setSlitSep(int slitSep) {
+        this.slitSep = slitSep;
+    }
+
+    public int getNbSlits() {
+        return this.nbSlits;
+    }
+
+    public void setNbSlits(int nbSlits) {
+        this.nbSlits = nbSlits;
+    }
+
+    public int getSlitWid() {
+        return this.slitWid;
+    }
+
+    public void setSlitWid(int slitWid) {
+        this.slitWid = slitWid;
+    }
+    
+    
 }

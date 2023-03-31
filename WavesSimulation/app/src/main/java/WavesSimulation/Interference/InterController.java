cfef0fd92dc73a7e64b2739fd6701dbf228fb9e4
darinahorescu2 <102605102+darinahorescu2@users.noreply.github.com>
2023-03-31 10:43:07 -0400

    Added faucet to window and finished the window

--- WavesSimulation/app/src/main/java/WavesSimulation/Interference/InterController.java
+++ WavesSimulation/app/src/main/java/WavesSimulation/Interference/InterController.java
@@ -1,10 +1,16 @@
 
 package WavesSimulation.Interference;
 
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import javafx.beans.value.ChangeListener;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
 /**
  *
@@ -13,7 +19,7 @@ import javafx.stage.Stage;
 public class InterController extends Stage{
    
     Stage owner; 
-    
+   
     @FXML
     private Slider sldFreq;
     
@@ -25,4 +31,81 @@ public class InterController extends Stage{
     
     @FXML
     private Pane animationPane;
+    
+    @FXML
+    private Pane backgroundPane;
+   
+    @FXML 
+    private Button btnStart;
+    
+    @FXML 
+    private Button btnStart2;
+    
+    @FXML
+    private Label lblTitle; 
+    
+    
+    ImageView imageFaucetBottom = new ImageView("/images/faucet.png");
+    ImageView imageFaucetTop = new ImageView("/images/faucet.png");
+    
+    
+    @FXML
+    public void handleBtnStart(ActionEvent event){
+        
+    }
+    
+    @FXML
+    public void handleBtnStart2(ActionEvent event){
+        
+    }
+    
+    public void initialize(){
+        
+        imageFaucetBottom.setFitHeight(140);
+        imageFaucetBottom.setFitWidth(140);
+        imageFaucetBottom.setTranslateX(160);
+        imageFaucetBottom.setTranslateY(490);
+        
+        imageFaucetTop.setFitHeight(140);
+        imageFaucetTop.setFitWidth(140);
+        imageFaucetTop.setTranslateX(160);
+        imageFaucetTop.setTranslateY(270);
+        
+        backgroundPane.getChildren().clear();
+        backgroundPane.getChildren().addAll(animationPane, lblTitle, imageFaucetBottom, imageFaucetTop, btnStart, btnStart2);
+    }
+    
+    public void getFreq(){
+        sldFreq.valueProperty().addListener(new ChangeListener<Number>(){
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue){
+                int freq; 
+                freq = (int)sldFreq.getValue();
+                System.out.println(freq);
+            }
+        }); 
+    }
+   
+    public void getAmp(){
+        
+        sldAmp.valueProperty().addListener(new ChangeListener<Number>(){
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue){
+                int amp;
+                amp = (int)sldAmp.getValue();
+                System.out.println(amp);
+            }
+        });   
+    }
+    
+    public void getSep(){
+        
+        sldSep.valueProperty().addListener(new ChangeListener<Number>(){
+            public void changed(ObservableValue<? extends Number> observable, Number oldValue, Number newValue){
+                int sep;
+                sep = (int)sldSep.getValue();
+                System.out.println(sep);
+            }
+        });   
+    }   
 }
+
+

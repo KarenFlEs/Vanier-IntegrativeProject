668e9ff2a250668c81668c11f89146d38f48d846
KarenFlEs <karen.florian6es@gmail.com>
2023-05-11 15:23:59 -0400

    Fixed the test class for diffraction part

--- WavesSimulation/app/src/test/java/WavesSimulationTest/DiffractionTest.java
+++ WavesSimulation/app/src/test/java/WavesSimulationTest/DiffractionTest.java
@@ -5,7 +5,6 @@ package WavesSimulationTest;
 
 import WavesSimulation.Diffraction.DiffractionSim;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 import org.junit.jupiter.api.Test;
 
 
@@ -13,27 +12,36 @@ class DiffractionTest {
     
     private final DiffractionSim diffSim = new DiffractionSim(); 
     
+    /**
+     * If the number is calculated, then it does not have a value of 0.00
+     * @param num
+     * @return 
+     */
     private boolean isCalculated (double num){
-        return num != 0.00; 
+        return num != 0.00 ; 
     }
     
-    @Test void diffAngleTest (){
-        System.out.println("Test Angles Calculation");
+    @Test void testDiffAngle (){
+        System.out.println("Test Angle Calculation");
         boolean expResult = true; 
         int wavelength = 380; 
-        double slitDistance = 0.5; 
+        double slitDistance = 10.0; 
         int orderNum = 1; 
+        System.out.println("Calculated Angle: " + diffSim.calculationAngle(wavelength, slitDistance, orderNum));
+        
         boolean calcResult = isCalculated(diffSim.calculationAngle(wavelength, slitDistance, orderNum)); 
         assertEquals (expResult, calcResult); 
-        fail("The test case is a prototype."); 
     }
     
     @Test void testDiffRadius (){
         System.out.println("Test Radius Calculation");
         boolean expResult = true; 
+        double angle = 0.217; 
+        diffSim.setDiffractionAngle(angle);
+        System.out.println("Calculated Radius: " + diffSim.calculationDiffractionRadius());
+        
         boolean calcResult = isCalculated(diffSim.calculationDiffractionRadius()); 
         assertEquals (expResult, calcResult); 
-        fail("The test case is a prototype."); 
     }
     
 }
